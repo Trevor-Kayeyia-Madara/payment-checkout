9e0bfa9e49943b9fe8af402afad1f50421e8c103
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-01-03 21:35:13 +0300

    Added Book, Search, Checkout and ThankYou files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Home from './components/home';
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Search from './components/Search';
+import Book from './components/Book';
+import Checkout from './components/Checkout';
+import ThankYou from './components/ThankYou';
 
 
 const App = () => (
   <div>
-  <Home />
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/book" element={<Book />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/thank-you" element={<ThankYou />} />
+    </Routes>
+  </Router>
   </div>
 );
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './home.css';
+
 
 const Home = () => (
   <div className="home">
@@ -26,22 +27,5 @@ const Home = () => (
     </div>
   </div>
 );
+export default Home;
 
-const Search = () => <h2>Search Page</h2>;
-const Book = () => <h2>Book Page</h2>;
-const Checkout = () => <h2>Checkout Page</h2>;
-const ThankYou = () => <h2>Thank You Page</h2>;
-
-const Path = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/book" element={<Book />} />
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/thank-you" element={<ThankYou />} />
-    </Routes>
-  </Router>
-);
-
-export default Path;
