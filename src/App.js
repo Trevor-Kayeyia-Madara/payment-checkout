5135f3ad1a5b41e0f4bd11c9d5203135fb59d8b7
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-01-03 19:01:04 +0300

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Home from './components/home';
 
 function App() {
   return (
     <div>
-  
+    <Home />
     </div>
   );
 }
---
+++ src/components/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './images/LE-GRAND-FOREST-logo.png';
+
+
+function Home() {
+  return (
+    <div>
+        <div class="logo">
+            <img src={logo} alt="logo" />
+            <h1>Hello World !</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
