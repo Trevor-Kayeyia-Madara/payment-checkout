9237c01b18d78ac2d397e1a895e4cf8e1eea9956
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-01-03 21:27:34 +0300

    Edited Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Home from './components/home';
 
-function App() {
-  return (
-    <div>
-    <Home />
-    </div>
-  );
-}
+
+
+
+const App = () => (
+  <div>
+  <Home />
+  </div>
+);
 
 export default App;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
-import logo from './images/LE-GRAND-FOREST-logo.png';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './home.css';
 
-
-function Home() {
-  return (
-    <div>
-        <div class="logo">
-            <img src={logo} alt="logo" />
-            <h1>Hello World !</h1>
-        </div>
+const Home = () => (
+  <div className="home">
+  
+    <div className="hero">
+      <h1>Search, Book, Checkout</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/search">1. Search</Link>
+          </li>
+          <li>
+            <Link to="/book">2. Book</Link>
+          </li>
+          <li>
+            <Link to="/checkout">3. Checkout</Link>
+          </li>
+          <li>
+            <Link to="/thank-you">4. Thank You</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
-}
+  </div>
+);
 
-export default Home
+const Search = () => <h2>Search Page</h2>;
+const Book = () => <h2>Book Page</h2>;
+const Checkout = () => <h2>Checkout Page</h2>;
+const ThankYou = () => <h2>Thank You Page</h2>;
+
+const Path = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/book" element={<Book />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/thank-you" element={<ThankYou />} />
+    </Routes>
+  </Router>
+);
+
+export default Path;
