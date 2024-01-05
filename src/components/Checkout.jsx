59c5db8a2feac4c7c0f6eb88cc0141d07b874720
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2024-01-05 13:03:47 +0300

    Done

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 
 function Checkout() {
+  const location = useLocation();
+  const { bookingDetails, extras, formData } = location.state || {};
+
+  if (!bookingDetails || !extras || !formData) {
+    // Handle the case where state is not available
+    return <p>No booking details found. Please go back and complete the booking.</p>;
+  }
+
+
+
   return (
-    <div>Checkout</div>
+    <div className='checkout'>
+      <div className="hero">
+        <h1>SEARCH</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/search"><span className=' circle'>1</span> Search</Link>
+            </li>
+            <li>
+              <Link to="/book"><span className='circle  active'>2</span> Book</Link>
+            </li>
+            <li>
+              <Link to="/checkout"><span className='circle'>3</span> Checkout</Link>
+            </li>
+            <li>
+              <Link to="/thank-you"><span className='circle'>4</span> Thank You</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   )
 }
 
